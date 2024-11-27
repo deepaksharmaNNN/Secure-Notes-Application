3e2d76bd5ad550dc662701cab09b7e2e94a9b815
Deepak Sharma <dsharma2828@gmail.com>
2024-11-28 00:00:30 +0530

    Secure Notes Project Started

---
+++ src/test/java/com/deepak/sharma/securenotes/SecureNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.sharma.securenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
