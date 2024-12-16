6bca13f5a73f5e1a94a94c42aa7941436dcb6384
Deepak Sharma <dsharma2828@gmail.com>
2024-12-16 12:34:10 +0530

    replaced project name from security Notes Application to secure notes application

---
+++ src/main/java/com/deepak/sharma/securenotes/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.deepak.sharma.securenotes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public
+}
---
+++ src/main/java/com/deepak/sharma/securenotes/service/UserService.java
@@ -0,0 +1,8 @@
+package com.deepak.sharma.securenotes.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+    
+}
